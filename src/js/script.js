1f2903116e72665f687de205bcc21d4b469f2cdc
DuttaAnirudh <worktodutta@gmail.com>
2024-02-12 22:15:18 +0530

    feat: add recommendation scroller on banner; add scrolling in y direction; add and design details page

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,26 @@
 // VARIABLES
 const searchBtn = document.querySelector('.search__icon');
 const searchField = document.querySelector('.search__field');
+const sliders = document.querySelectorAll('#slider');
 
 // EVENT LISTENERS
 // Focusing on search input field when clicking on search icon
 searchBtn.addEventListener('click', () => {
   searchField.focus();
+});
+
+// Scrolling in X direction on mouse scroll
+sliders.forEach(slider => {
+  slider.addEventListener('wheel', function (e) {
+    // Determing scroll direction(1 = down | -1 = up)
+    const delta = Math.sign(e.deltaY);
+
+    // Controling the scroll speed
+    const scrollAmount = 25;
+
+    slider.scrollLeft += delta * scrollAmount;
+
+    // Prevent Default scrolling behavior
+    e.preventDefault();
+  });
 });
